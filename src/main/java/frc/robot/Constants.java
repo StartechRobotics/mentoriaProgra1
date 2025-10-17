de3028a21aeb30eb3a71137305effc9746bed2fd
Abbbb ffffff <sabfems@gmail.com>
2025-10-17 15:23:19 -0600

    constants and units

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public class Constants {
+    public final static int kRight1 = 1;
+    public final static int kRight2 = 2;
+    public final static int kLeft1 = 3;
+    public final static int kLeft2 = 4;
+
+    public final static int kDriverControllerPort = 0;
+
+    public final static int kMeterSecondProportion = 3;
+    public final static double kRadProportion = Math.PI;
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,16 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Radians;
+
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -19,32 +25,58 @@ public class Robot extends TimedRobot {
   //LO QUE ESTA AFUERA ES UNIVERSAL (EXISTE DESDE ANTES DE ENCENDER EL ROBOT)
   //Paso 1: Crear los motores
   // Creando una variable (de tipo sparkmax) para cada motor
+  
   public SparkMax motorDerecho1;
   public SparkMax motorDerecho2;
+  public SparkMaxConfig RightConfig1;
+  public SparkMaxConfig RightConfig2;
+
   public SparkMax motorIzquierdo1;
   public SparkMax motorIzquierdo2;
+  public SparkMaxConfig LeftConfig2;
+
+  public XboxController control = new XboxController(Constants.kDriverControllerPort);
 
   public Robot() { //ESTO SE EJECUTA AL INICIO DEL ROBOT (CUANDO SE ENCIENDE / ACTIVA)
 
     //Paso 2: Inicializar los motores
     // Cada motor se inicia con la funcion [new SparkMax(numeroPuerto, tipoMotor)]
     // donde puerto es el CAN ID y tipoMotor es si es brushed o brushless
-    motorDerecho1 = new SparkMax(1, MotorType.kBrushed);
-    motorDerecho2 = new SparkMax(2, MotorType.kBrushed);
+    motorDerecho1 = new SparkMax(Constants.kRight1, MotorType.kBrushed);
+    motorDerecho2 = new SparkMax(Constants.kRight2, MotorType.kBrushed);
+    RightConfig1 = new SparkMaxConfig();
+    RightConfig2 = new SparkMaxConfig();
+    RightConfig1.inverted(true);
+    RightConfig2.follow(motorDerecho1);
+    motorDerecho1.configure(RightConfig1, null, null);
+    motorDerecho2.configure(RightConfig2, null, null);
 
-    motorIzquierdo1 = new SparkMax(3, MotorType.kBrushed);
-    motorIzquierdo2 = new SparkMax(4, MotorType.kBrushed);
+    motorIzquierdo1 = new SparkMax(Constants.kLeft1, MotorType.kBrushed);
+    motorIzquierdo2 = new SparkMax(Constants.kLeft2, MotorType.kBrushed);
+    LeftConfig2 = new SparkMaxConfig();
+    LeftConfig2.follow(motorIzquierdo1);
+    motorIzquierdo2.configure(LeftConfig2, null, null);
 
   }
   @Override
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    drive(new ChassisSpeeds(
+      -control.getLeftY()*Constants.kMeterSecondProportion, //SpeedX(Y) = -Y*kMeterProportion
+      0.0,
+      control.getRightX() * Constants.kRadProportion));
+  }
 
-  @Override
-  public void simulationInit() {}
+  // Escribir un metodo Drive que tome como parametros la velocidad V y velocidad angular W
+  public void drive(ChassisSpeeds speeds){
+    double V = speeds.vxMetersPerSecond;
+    double W = speeds.omegaRadiansPerSecond;
+    double speedR = V+W*180/Math.PI;
+    double speedL = V-W*180/Math.PI; 
+    motorDerecho1.set(speedR);
+    motorIzquierdo1.set(speedL); 
+  }
 
-  @Override
-  public void simulationPeriodic() {}
 }
